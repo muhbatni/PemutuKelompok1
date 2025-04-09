2de4763889aba160246380cc98f8da377f1f4664
Ahmad Ghozi Darmawan <ahmadghozi106@gmail.com>
2025-04-09 10:17:02 +0700

    merubah base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/web-ci/';
+    public string $baseURL = 'http://localhost/pemutu';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
