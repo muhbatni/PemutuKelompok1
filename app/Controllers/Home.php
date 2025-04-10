1423b81bded523576086893f152e4b2f4af929e7
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-10 23:38:42 +0700

    refactor: mengubah judul

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,12 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        $data["title"] = "Judul";
-        echo view('layouts/header.php', $data);
-        echo view('layouts/tables.php');
-        echo view('layouts/footer.php');
-    }
+  public function index()
+  {
+    $data["title"] = "Beranda";
+    echo view('layouts/header.php', $data);
+    echo view('layouts/tables.php');
+    echo view('layouts/footer.php');
+  }
 
 }
