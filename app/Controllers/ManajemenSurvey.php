963b59e3f819c64335cf9fa3608a722000f2b7ba
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-18 06:33:27 +0700

    refactor(route): change route resource location

--- app/Controllers/ManajemenSurvey.php
+++ app/Controllers/ManajemenSurvey.php
@@ -5,7 +5,7 @@ class ManajemenSurvey extends BaseController
   public function index()
   {
     echo view('layouts/header.php', ["title" => "Manajemen Survey"]);
-    echo view('survey_kepuasan/buat_survey/index.php');
+    echo view('survey_kepuasan/manajemen_survey/index.php');
     echo view('layouts/footer.php');
   }
 }
