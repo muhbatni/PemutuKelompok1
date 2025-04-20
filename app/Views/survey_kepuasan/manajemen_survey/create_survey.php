71458d0c45a28aea5ec45910c98e0108d3870620
Hafid Aji Fauzan <hafidaji93@gmail.com>
2025-04-20 18:35:07 +0700

    style(pertanyaan): memperbaiki ui create pertanyaan

--- app/Views/survey_kepuasan/manajemen_survey/create_survey.php
+++ app/Views/survey_kepuasan/manajemen_survey/create_survey.php
@@ -52,65 +52,63 @@
         </div>
         <div class="form-group m-form__group row">
           <label class="col-form-label col-lg-3 col-sm-12">List Pertanyaan</label>
-          <div class="row ui-sortable" id="m_sortable_portlets">
-            <div class="col-lg-12 portlet-template">
-              <div
-                class="m-portlet m-portlet--tabs m-portlet--success m-portlet--head-solid-bg m-portlet--sortable m-portlet--bordered"
-                style="">
-                <div class="m-portlet__head ui-sortable-handle">
-                  <div class="m-portlet__head-caption">
-                    <div class="m-portlet__head-title">
-                      <h3 class="m-portlet__head-text">
-                        Jenis Pertanyaan
-                      </h3>
+          <div class="col-lg-7 col-md-7 col-sm-12">
+            <div class="row ui-sortable" id="m_sortable_portlets">
+              <div class="col-lg-12 portlet-template">
+                <div class="m-portlet m-portlet--mobile m-portlet--sortable m-portlet--bordered" style="">
+                  <div class="m-portlet__head ui-sortable-handle">
+                    <div class="m-portlet__head-caption">
+                      <div class="m-portlet__head-title">
+                        <h3 class="m-portlet__head-text">
+                          Jenis Pertanyaan
+                        </h3>
+                      </div>
+                    </div>
+                    <div class="m-portlet__head-tools">
+                      <ul
+                        class="nav nav-tabs m-tabs m-tabs-line m-tabs-line--brand  m-tabs-line--right m-tabs-line-danger"
+                        role="tablist">
+                        <li class="nav-item m-tabs__item">
+                          <a class="nav-link m-tabs__link active" data-toggle="tab"
+                            href="#m_portlet_base_demo_1_tab_content" role="tab">
+                            <i class="la la-star"></i> Opsian
+                          </a>
+                        </li>
+                        <li class="nav-item m-tabs__item">
+                          <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_portlet_base_demo_1_tab_content"
+                            role="tab" aria-selected="false">
+                            <i class="la la-pencil-square"></i>
+                            Isian
+                          </a>
+                        </li>
+                      </ul>
                     </div>
                   </div>
-                  <div class="m-portlet__head-tools">
-                    <ul class="nav nav-tabs m-tabs m-tabs-line  m-tabs-line--right" role="tablist">
-                      <li class="nav-item m-tabs__item">
-                        <a class="nav-link m-tabs__link active" data-toggle="tab"
-                          href="#m_portlet_base_demo_1_tab_content" role="tab">
-                          <i class="la la-star"></i> Opsian
-                        </a>
-                      </li>
-                      <li class="nav-item m-tabs__item">
-                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#m_portlet_base_demo_1_tab_content"
-                          role="tab" aria-selected="false">
-                          <i class="la la-pencil-square"></i>
-                          Isian
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="m-portlet__body">
-                  <div class="d-flex align-items-center justify-content-center">
-                    <input type="text" name="pertanyaan[]" class="form-control m-input" id="exampleInputTitle1"
-                      aria-describedby="emailHelp" placeholder="Isi Pertanyaan" fdprocessedid="8wf9oh">
-                    <input type="hidden" name="jenis[]" value="1">
-                  </div>
-                  <!-- <div class="md-editor" id="1745128214339"></div>
-                  <textarea name="markdown" class="form-control md-input" data-provide="markdown" rows="5"
-                    style="resize: none;" aria-describedby="markdown-error" aria-invalid="true"></textarea> -->
-                  <br>
-                  <div data-repeater-delete="" class="btn-sm btn btn-danger m-btn m-btn--icon m-btn--pill">
-                    <span>
-                      <i class="la la-trash-o"></i>
-                      <span>Delete</span>
-                    </span>
+                  <div class="m-portlet__body">
+                    <div class="d-flex align-items-center justify-content-center">
+                      <input type="text" name="pertanyaan[]" class="form-control m-input" id="exampleInputTitle1"
+                        aria-describedby="emailHelp" placeholder="Isi Pertanyaan" fdprocessedid="8wf9oh">
+                      <input type="hidden" name="jenis[]" value="1">
+                    </div>
+                    <br>
+                    <div data-repeater-delete="" class="btn-sm btn btn-danger m-btn m-btn--icon m-btn--pill">
+                      <span>
+                        <i class="la la-trash-o"></i>
+                        <span>Delete</span>
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="m-form__group form-group row">
-            <label class="col-lg-2 col-form-label"></label>
-            <div class="col-lg-4">
-              <div data-repeater-create="" class="btn btn btn-sm btn-brand m-btn m-btn--icon m-btn--pill m-btn--wide">
-                <span>
-                  <i class="la la-plus"></i>
-                  <span>Add</span>
-                </span>
+            <div class="form-group row">
+              <div class="col-lg-4">
+                <div data-repeater-create="" class="btn btn btn-sm btn-brand m-btn m-btn--icon m-btn--pill m-btn--wide">
+                  <span>
+                    <i class="la la-plus"></i>
+                    <span>Add</span>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
