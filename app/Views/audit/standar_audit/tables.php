e429621b6c92cf159e7fbf01c89d4ea3479dc425
Ahmad Ghozi Darmawan <ahmadghozi106@gmail.com>
2025-04-21 14:27:40 +0700

    fix(standar): perbaiki href standar audit

--- app/Views/audit/standar_audit/tables.php
+++ app/Views/audit/standar_audit/tables.php
@@ -5,7 +5,7 @@
       <div class="m-portlet__head-caption">
         <div class="m-portlet__head-title">
           <h3 class="m-portlet__head-text">
-            Datatable initilized from HTML table
+            Tabel Standar Audit
           </h3>
         </div>
       </div>
