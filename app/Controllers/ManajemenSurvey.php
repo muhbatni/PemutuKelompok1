3cd00db8541926b180b8bfda479967e90b00c1e9
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-21 22:36:14 +0700

    feat(validation): add validation for survey

--- app/Controllers/ManajemenSurvey.php
+++ app/Controllers/ManajemenSurvey.php
@@ -4,17 +4,18 @@ namespace App\Controllers;
 use App\Models\SurveyModel;
 use App\Models\PertanyaanSurveyModel;
 use App\Models\PelaksanaanSurveyModel;
-use App\Models\PeriodeModel;
+use \Config\Database;
+
 class ManajemenSurvey extends BaseController
 {
   protected $surveyModel;
-  protected $periodeModel;
   protected $pelaksanaanSurveyModel;
   protected $pertanyaanSurveyModel;
+  protected $helpers = ['surveys'];
+
   public function __construct()
   {
     $this->surveyModel = new SurveyModel();
-    $this->periodeModel = new PeriodeModel();
     $this->pelaksanaanSurveyModel = new PelaksanaanSurveyModel();
     $this->pertanyaanSurveyModel = new PertanyaanSurveyModel();
   }
@@ -23,7 +24,6 @@ class ManajemenSurvey extends BaseController
   {
     $data['surveys'] = $this->surveyModel->paginate(10);
     $data['pager'] = $this->surveyModel->pager;
-
     echo view('layouts/header.php', ["title" => "Manajemen Survey"]);
     echo view('survey_kepuasan/manajemen_survey/index.php', $data);
     echo view('layouts/footer.php');
@@ -32,75 +32,74 @@ class ManajemenSurvey extends BaseController
   public function createSurvey()
   {
     if ($this->request->getMethod() === "POST") {
-      $data = [
-        'kode' => $this->request->getPost('kode_survey') ?: null,
-        'nama' => $this->request->getPost('nama_survey') ?: null,
-        'dokumen_pendukung' => $this->request->getPost('dokumen_pendukung_survey'),
-        'status' => $this->request->getPost('status_survey') === "true" ? true : false,
-
-      ];
-      $id_survey = $this->surveyModel->insert($data);
-      $id_periode = $this->request->getPost('id_periode') ?: null;
-      if ($id_periode) {
-
-        $this->pelaksanaanSurveyModel->insert([
-          'id_periode' => $id_periode,
-          'id_survey' => $id_survey,
-          'tanggal_mulai' => $this->request->getPost('tanggal_mulai') ?: null,
-          'tanggal_selesai' => $this->request->getPost('tanggal_selesai') ?: null,
-          'deskripsi' => $this->request->getPost('deskripsi') ?: null,
-          'created_at' => date('Y-m-d H:i:s'),
-        ]);
+      $validation = service('validation');
+      $data = $this->request->getPost();
+      $data['dokumen_pendukung_survey'] = $this->request->getFile('dokumen_pendukung_survey');
+      if (!$validation->run($data, 'surveys')) {
+        echo view("layouts/header.php", ["title" => "Manajemen Survey"]);
+        echo view(
+          "survey_kepuasan/manajemen_survey/create_survey.php",
+          ['errors' => $validation->getErrors(), 'old' => $data]
+        );
+        echo view("layouts/footer.php");
+        return null;
       }
-      $pertanyaan = $this->request->getPost('pertanyaan');
-      $jenis = $this->request->getPost('jenis');
-
-      if ($pertanyaan && $jenis) {
-        $pertanyaanData = [];
-        foreach ($pertanyaan as $index => $teks) {
-          $jenisValue = isset($jenis[$index]) && is_numeric($jenis[$index]) ? (int) $jenis[$index] : null;
-          if ($jenisValue === null || empty($teks)) {
-            continue;
-          }
-          $pertanyaanData[] = [
-            'id_survey' => $id_survey,
-            'teks' => $teks,
-            'jenis' => $jenis[$index],
-            'is_aktif' => true,
-            'updated_at' => date('Y-m-d H:i:s'),
-          ];
-        }
-        if (empty($pertanyaanData)) {
-          return redirect()->to(base_url('public/survey/manajemen-survey'))->with('error', 'Tidak ada pertanyaan yang valid!');
-        }
-        $this->pertanyaanSurveyModel->insertBatch($pertanyaanData);
+      $database = Database::connect();
+      $database->transStart();
+      $data['id_survey'] = createSurveyData($database, 's_survey', [
+        'kode' => $data['kode_survey'] ?: null,
+        'nama' => $data['nama_survey'] ?: null,
+        'dokumen_pendukung' => $data['dokumen_pendukung_survey'] ?: null,
+        'status' => $data['status_survey'] === "true" ? true : false,
+      ]);
+      if (!$data['id_survey']) {
+        $database->transRollback();
+        $database->close();
+        return;
       }
-      return redirect()->to(base_url('public/survey/manajemen-survey'))->with('success', 'Survey berhasil dibuat!');
+      $result = createSurveyData($database, 's_pelaksanaan_survey', [
+        'id' => $data['id_survey'] ?: null,
+        'id_periode' => $data['id_periode'] ?: null,
+        'tanggal_mulai' => $data['tanggal_mulai'] ?: null,
+        'tanggal_selesai' => $data['tanggal_selesai'] ?: null,
+        'deskripsi' => $data['deskripsi_survey'] ?: null,
+        'created_at' => date('Y-m-d H:i:s'),
+      ]);
+      if (!$result) {
+        $database->transRollback();
+        $database->close();
+        return;
+      }
+      $result = createPertanyaanData($database, $data);
+      if (!$result) {
+        $database->transRollback();
+        $database->close();
+        return;
+      }
+      $database->transCommit();
+      $database->close();
+      return alert('survey/manajemen-survey', 'success', 'Survey berhasil dibuat!');
     }
-
-    // Ambil data periode
-    $data['periode'] = $this->periodeModel->findAll();
-
     echo view('layouts/header.php', ["title" => "Manajemen Survey"]);
-    echo view('survey_kepuasan/manajemen_survey/create_survey.php', $data);
+    echo view('survey_kepuasan/manajemen_survey/create_survey.php');
     echo view('layouts/footer.php');
   }
 
   public function editSurvey($id_survey)
   {
     if ($this->request->getMethod() === "POST") {
-      $data = [
-        'kode' => $this->request->getPost('kode_survey') ?: null,
-        'nama' => $this->request->getPost('nama_survey') ?: null,
-        'dokumen_pendukung' => $this->request->getPost('dokumen_pendukung_survey'),
-        'status' => $this->request->getPost('status_survey') === "true" ? true : false
-      ];
-      $this->surveyModel->update($id_survey, $data);
-      return redirect()->to(base_url('public/survey/manajemen-survey'))->with('success', 'Survey berhasil diedit!');
+      $data = $this->request->getPost();
+      $this->surveyModel->update($id_survey, [
+        'kode' => $data['kode_survey'] ?: null,
+        'nama' => $data['nama_survey'] ?: null,
+        'dokumen_pendukung' => $data['dokumen_pendukung_survey'] ?: null,
+        'status' => $data['status_survey'] === "true" ? true : false
+      ]);
+      return alert('survey/manajemen-survey', 'success', 'Survey berhasil diedit!');
     }
     $data['survey'] = $this->surveyModel->find($id_survey);
     if (!$data['survey']) {
-      return redirect()->to(base_url('public/survey/manajemen-survey'))->with('error', 'Survey tidak ditemukan!');
+      return alert('survey/manajemen-survey', 'error', 'Survey tidak ditemukan!');
     }
     echo view('layouts/header.php', ["title" => "Manajemen Survey"]);
     echo view('survey_kepuasan/manajemen_survey/edit_survey.php', $data);
@@ -113,7 +112,7 @@ class ManajemenSurvey extends BaseController
       return;
     }
     if (!$this->surveyModel->delete($id_survey)) {
-      return;
+      return redirect()->to(base_url('public/survey/manajemen-survey'))->with('error', 'Survey gagal dihapus!');
     }
     return redirect()->to(base_url('public/survey/manajemen-survey'))->with('success', 'Survey berhasil dihapus!');
   }
