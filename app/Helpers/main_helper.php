38ce798938d6c0f1aceeb92564d4e39374b8041a
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-21 22:31:22 +0700

    chore(helpers): add and rename files

---
+++ app/Helpers/main_helper.php
@@ -0,0 +1,8 @@
+<?php
+
+function alert($redirectPath, $type, $message)
+{
+  return redirect()->to(base_url('public/' . $redirectPath))->with($type, $message);
+}
+
+?>
---
+++ app/Helpers/surveys_helper.php
@@ -0,0 +1,38 @@
+<?php
+
+function createSurveyData($database, $tableName, $data)
+{
+  $pQuery = $database->prepare(static fn($database) => $database->table($tableName)->insert($data));
+  $pQuery->execute(...array_values($data));
+  return $database->insertID();
+}
+
+function createPertanyaanData($database, $data)
+{
+  $idSurvey = $data['id_survey'] ?: null;
+  $pertanyaan = $data['pertanyaan'] ?: null;
+  $jenis = $data['jenis'] ?: null;
+  if (!$idSurvey || !$pertanyaan || !$jenis) {
+    return null;
+  }
+  $pertanyaanData = [];
+  foreach ($pertanyaan as $index => $teks) {
+    $jenisValue = isset($jenis[$index]) && is_numeric($jenis[$index]) ? (int) $jenis[$index] : null;
+    if ($jenisValue === null || empty($teks)) {
+      continue;
+    }
+    $pertanyaanData[] = [
+      'id_survey' => $idSurvey,
+      'teks' => $teks,
+      'jenis' => $jenis[$index],
+      'is_aktif' => true,
+      'updated_at' => date('Y-m-d H:i:s'),
+    ];
+  }
+  if (empty($pertanyaanData)) {
+    return null;
+  }
+  $database->table('s_pertanyaan')->insertBatch($pertanyaanData);
+  return $database->insertID();
+}
+?>
