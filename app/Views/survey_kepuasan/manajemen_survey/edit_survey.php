5374d55234e0bf0fc657fc79e20963494cb2dd1e
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-25 22:03:13 +0700

    fix(survey): textarea start with spaces

--- app/Views/survey_kepuasan/manajemen_survey/edit_survey.php
+++ app/Views/survey_kepuasan/manajemen_survey/edit_survey.php
@@ -81,9 +81,8 @@
           <div class="form-group m-form__group row">
             <label class="col-form-label col-lg-3 col-sm-12">Deskripsi<span style="color: red">*</span></label>
             <div class="col-lg-7 col-md-7 col-sm-12">
-              <textarea class="form-control" id="deskripsi" name="deskripsi_survey" rows="3" required>
-                  <?= isset($pelaksanaan_survey['deskripsi']) ? $pelaksanaan_survey['deskripsi'] : '' ?>
-            </textarea>
+              <textarea class="form-control" id="deskripsi" name="deskripsi_survey" rows="3"
+                required><?= isset($pelaksanaan_survey['deskripsi']) ? $pelaksanaan_survey['deskripsi'] : '' ?></textarea>
             </div>
           </div>
           <div class="form-group m-form__group row">
