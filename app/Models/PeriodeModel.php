4728225405151f0ecf8ed13a5dd163c57249d6d9
Hafid Aji Fauzan <hafidaji93@gmail.com>
2025-04-26 22:35:17 +0700

    merge: merge get periodes

--- app/Models/PeriodeModel.php
+++ app/Models/PeriodeModel.php
@@ -6,18 +6,6 @@ use CodeIgniter\Model;
 
 class PeriodeModel extends Model
 {
-<<<<<<< HEAD
-    protected $table = 'm_periode'; // Sesuaikan dengan nama tabel di database
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['tahun', 'ts'];
-
-    public function getPeriodes()
-    {
-        // Ambil data periode dari database
-        return $this->select('id, tahun, ts')->findAll();
-    }
-}
-=======
   protected $table = 'm_periode';
   protected $primaryKey = 'id';
   protected $allowedFields = ['tahun', 'ts'];
@@ -27,4 +15,3 @@ class PeriodeModel extends Model
     return $this->findAll();
   }
 }
->>>>>>> 4e213614bcc5e1bffaf4fa4e1ed8b3c8bad3aa59
