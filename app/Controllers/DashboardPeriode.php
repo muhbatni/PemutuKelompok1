6f35bf77ccf1ac13eed66ac3315c88e57da7496d
aderhans <adeerhans@gmail.com>
2025-04-26 19:35:49 +0700

    fix(dashboard): perbaikan dropdown filter tahun

--- app/Controllers/DashboardPeriode.php
+++ app/Controllers/DashboardPeriode.php
@@ -2,16 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\PeriodeModel;
+
 class DashboardPeriode extends BaseController
 {
   public function index()
   {
-    $data["user_type"] = "Mahasiswa";
-    $data["title"] = "Dashboard";
-    echo view('layouts/header.php', $data);
-    echo view('akreditasi/dashboard/form.php');
-    echo view('layouts/footer.php');
-  }
+    $periodeModel = new PeriodeModel();
+    $periodeList = $periodeModel->findAll();
 
+    $data = [
+      "user_type" => "Mahasiswa",
+      "title" => "Dashboard",
+      "periodeList" => $periodeList,
+    ];
+
+    echo view('layouts/header.php', $data);
+    echo view('akreditasi/dashboard/form.php', $data);
+    echo view('layouts/footer.php', $data);
+  }
 }
 ?>
