cdf4815fbc0ad2bf496d94cfe12aa9a2f6c5b101
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-30 09:53:57 +0700

    feat(survey): add getPaginatedSurveys function

--- app/Models/SurveyModel.php
+++ app/Models/SurveyModel.php
@@ -12,14 +12,25 @@ class SurveyModel extends Model
   protected $allowedFields = ['id_kriteria', 'kode', 'nama', 'dokumen_pendukung', 'status'];
   protected $returnType = 'array';
 
+  public function getPaginatedSurveys($perPage = 10, $page = null)
+  {
+    $this->select('s_survey.*, s_pelaksanaan_survey.tanggal_mulai, s_pelaksanaan_survey.tanggal_selesai')
+      ->join('s_pelaksanaan_survey', 's_pelaksanaan_survey.id = s_survey.id');
+    $surveys = $this->paginate($perPage, 'default', $page);
+    $pager = $this->pager;
+    return [
+      'surveys' => $surveys,
+      'pager' => $pager
+    ];
+  }
+
   public function getActiveSurveys()
   {
-      $tanggalSekarang = date('Y-m-d');
-      echo "<script>console.log('$tanggalSekarang');</script>";
-      return $this->select('s_survey.*, s_pelaksanaan_survey.tanggal_mulai, s_pelaksanaan_survey.tanggal_selesai')
-                  ->join('s_pelaksanaan_survey', 's_pelaksanaan_survey.id = s_survey.id')
-                  ->where('tanggal_mulai <=', $tanggalSekarang)
-                  ->where('tanggal_selesai >=', $tanggalSekarang)
-                  ->findAll();
+    $tanggalSekarang = date('Y-m-d');
+    return $this->select('s_survey.*, s_pelaksanaan_survey.tanggal_mulai, s_pelaksanaan_survey.tanggal_selesai')
+      ->join('s_pelaksanaan_survey', 's_pelaksanaan_survey.id = s_survey.id')
+      ->where('tanggal_mulai <=', $tanggalSekarang)
+      ->where('tanggal_selesai >=', $tanggalSekarang)
+      ->findAll();
   }
 }
