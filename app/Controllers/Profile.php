a49963d6bf3978e5db2e2b065dd9b5b0a5339a7d
Achmad Raihan <totsukamail3278@gmail.com>
2025-04-30 17:47:46 +0700

    refactor(profile): clean up codes

--- app/Controllers/Profile.php
+++ app/Controllers/Profile.php
@@ -31,12 +31,36 @@ class Profile extends BaseController
     }
   }
 
+  private function isDataValid($data, $file)
+  {
+    $validation = service('validation');
+    if (empty($data)) {
+      return redirect()->back()->with('error', 'Tidak ada data untuk diperbarui!');
+    }
+
+    if ($file && $file->isValid()) {
+      $data['foto'] = pg_escape_bytea(file_get_contents($file->getTempName()));
+    }
+
+    if (!$validation->run($data, "profile")) {
+      echo view('layouts/header', ['title' => 'Profile']);
+      echo view(
+        'profile/index',
+        [
+          'errors' => $validation->getErrors(),
+          'old' => $data
+        ]
+      );
+      echo view('layouts/footer');
+      return;
+    }
+  }
+
   public function edit()
   {
     if ($this->request->getMethod() !== 'POST') {
       return;
     }
-    $validation = service('validation');
     $token = getDatabyToken();
     if (!$token) {
       return null;
@@ -46,34 +70,20 @@ class Profile extends BaseController
     $postData = $this->request->getPost();
     $data = ['nama' => $postData['nama']];
     $file = $this->request->getFile('avatar');
-    if ($file && $file->isValid()) {
-      $data['foto'] = pg_escape_bytea(file_get_contents($file->getTempName()));
-    }
-    if (!$this->validate("profile")) {
-      echo view('layouts/header', ['title' => 'Profile']);
-      echo view(
-        'profile/index',
-        [
-          'errors' => $validation->getErrors(),
-          'old' => $postData
-        ]
-      );
-      echo view('layouts/footer');
-      return;
-    }
 
     $data = array_filter($data, fn($value) => !$value && $value !== '');
-    if (empty($data)) {
-      return redirect()->back()->with('error', 'No data to update.');
+
+    if (!$this->isDataValid($data, $file)) {
+      return;
     }
 
     $this->handleCacheImage($data['foto'], $userId);
 
     $isUpdate = $this->userModel->update($user['id'], $data);
     if (!$isUpdate) {
-      return redirectWithMessage('profile', 'error', 'Failed to update the profile!');
+      return redirectWithMessage('profile', 'error', 'Gagal memperbarui profil!');
     }
-    return redirectWithMessage('profile', 'success', 'Profile updated successfully!');
+    return redirectWithMessage('profile', 'success', 'Berhasil memperbarui profil!');
   }
 
   public function reset_password()
