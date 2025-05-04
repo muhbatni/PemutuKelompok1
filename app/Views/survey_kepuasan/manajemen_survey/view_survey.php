0668c0aa068f4cff5a7121e44d91c4dd49821a4f
Achmad Raihan <totsukamail3278@gmail.com>
2025-05-04 18:07:59 +0700

    fix(survey): validate if the optionsData exists

--- app/Views/survey_kepuasan/manajemen_survey/view_survey.php
+++ app/Views/survey_kepuasan/manajemen_survey/view_survey.php
@@ -42,18 +42,6 @@
 <script>
   const questionIDs = [];
   const answers = [];
-  <?php
-  if (isset($optionsData)) {
-    foreach ($optionsData as $data): ?>
-      questionIDs.push(<?= $data['id_pertanyaan'] ?>);
-      answers.push(<?= json_encode($data['jawaban']); ?>);
-      <?php
-    endforeach;
-  }
-  ?>
-  if (!answers || !questionIDs) {
-    return;
-  }
   const answerLookUp = {
     '1': 'Sangat Buruk',
     '2': 'Buruk',
@@ -61,59 +49,68 @@
     '4': 'Baik',
     '5': 'Sangat Baik'
   };
-  const labels = [];
-  const data = [];
-  answers.forEach(answer => {
-    labels.push(answer.map(item => answerLookUp[item.jawaban]));
-    data.push(answer.map(item => item.jumlah));
-  });
-  questionIDs.forEach((id, index) => {
-    let ctx = document.getElementById(`P_${id}`).getContext('2d');
-    let surveyChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: labels[index],
-        datasets: [{
-          label: 'Data Kepuasan',
-          data: data[index],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: 'Kategori Jawaban'
-            }
-          },
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Jumlah Jawaban'
+  <?php
+  if (isset($optionsData)):
+    foreach ($optionsData as $data): ?>
+      questionIDs.push(<?= $data['id_pertanyaan'] ?>);
+      answers.push(<?= json_encode($data['jawaban']); ?>);
+    <?php endforeach; ?>
+    const labels = [];
+    const data = [];
+    answers.forEach(answer => {
+      labels.push(answer.map(item => answerLookUp[item.jawaban]));
+      data.push(answer.map(item => item.jumlah));
+    });
+    questionIDs.forEach((id, index) => {
+      let ctx = document.getElementById(`P_${id}`).getContext('2d');
+      let surveyChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels[index],
+          datasets: [{
+            label: 'Data Kepuasan',
+            data: data[index],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 205, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 159, 64)',
+              'rgb(255, 205, 86)',
+              'rgb(75, 192, 192)',
+              'rgb(54, 162, 235)',
+              'rgb(153, 102, 255)',
+              'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            x: {
+              title: {
+                display: true,
+                text: 'Kategori Jawaban'
+              }
+            },
+            y: {
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Jumlah Jawaban'
+              }
             }
           }
         }
-      }
+      });
     });
-  });
+    <?php
+  endif;
+  ?>
 </script>
