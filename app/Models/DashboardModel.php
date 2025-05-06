a433da0d9aadd65a740a7a9eaf7964cac3040596
aderhans <adeerhans@gmail.com>
2025-05-06 17:16:20 +0700

    feat: view dashboard

--- app/Models/DashboardModel.php
+++ app/Models/DashboardModel.php
@@ -13,10 +13,11 @@ class DashboardModel extends Model
     {
         $builder = $this->db->table('p_unit_pemutu pu');
         $builder->select('pu.id, mu.nama as nama_unit, 
-                         ip.kondisi, 
-                         isi.isian as status_isian,
-                         isi.status as status_aktif,
-                         pu.id_periode, mp.tahun');
+                     mp.tahun as periode,
+                     mp.ts as ts, 
+                     isi.isian as status_isian,
+                     isi.status as status_aktif,
+                     pu.id_periode');
         $builder->join('m_unit mu', 'pu.id_unit = mu.id', 'left');
         $builder->join('p_isian_pemutu isi', 'isi.id_unitpemutu = pu.id', 'left');
         $builder->join('p_instrumen_pemutu ip', 'isi.id_instrumen = ip.id', 'left');
@@ -62,7 +63,7 @@ class DashboardModel extends Model
     public function getAllPeriode()
     {
         $builder = $this->db->table('m_periode');
-        $builder->select('*');
+        $builder->select('id, tahun, ts');
         $builder->orderBy('tahun', 'DESC');
         return $builder->get()->getResultArray();
     }
